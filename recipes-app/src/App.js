976380f38973f902a1d51b0d394a47f7b933c397
sunlinMain <sunzifeng@simverse.cn>
2022-10-24 00:10:42 +0800

    init

--- recipes-app/src/App.js
+++ recipes-app/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import {Button} from 'antd-mobile'
+import { AntOutline } from 'antd-mobile-icons'
+import appstyle from './styles/app.module.css'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button color='primary' fill='none' className={appstyle.login}>
+          <AntOutline fontSize={12} />
+          <span>登录</span>
+          </Button>
+        <Button color='success' fill='none'>2</Button>
+        <Button color='danger' fill='none'>3</Button>
+        <Button color='warning' fill='none'>4</Button>
+        <Button color='Default' fill='none'>5</Button>
+      </div>
+    )
+  }
 }
-
-export default App;
